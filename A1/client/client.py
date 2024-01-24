ac4ab835aa556dfd7822c5dfc1fee2b915c33cbb
Kushaz Sehgal <kushaz.sehgal@gmail.com>
2024-01-24 20:06:54 +0530

    Client modified

--- A1/client/client.py
+++ A1/client/client.py
@@ -1,5 +1,16 @@
 import requests
+import aiohttp
+import asyncio
+import random
+import re
+import matplotlib.pyplot as plt
 
+def extract_serv_id(input_string):
+    match = re.search(r"Hello from Server: (\S+)", input_string)
+    if match:
+        return match.group(1)  # This is the server ID
+    else:
+        return None
 def add(n = 1, hostnames = ["s1"]):
     r = requests.post('http://localhost:5000/add', json={"n": n, "hostnames": hostnames})
     print(r.json())
@@ -8,19 +19,38 @@ def rm(n = 1, hostnames = ["s1"]):
     r = requests.delete('http://localhost:5000/rm', json={"n": n, "hostnames": hostnames})
     print(r.json())
 
-def rep():
-    r = requests.get('http://localhost:5000/rep')
-    print(r.json())
+async def request(session, url):
+    # Example of an async GET request
+    async with session.get(url) as response:
+        return await response.json()
 
-def home():
-    r = requests.get('http://localhost:5000/home')
-    print(r.json())
 
-def heartbeat():
-    r = requests.get('http://localhost:5000/heartbeat')
-    print(r.json())
+async def main():
+    url = 'http://localhost:5000/home'
+    responses = None
+    dic = {}
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for _ in range(10):
+            # generate a random url from urls list
+            tasks.append(request(session, url))
+        responses = await asyncio.gather(*tasks)
+        print(responses)
+    for response in responses:
+        message = response['message']
+        serv_id = extract_serv_id(message)
+        print(f"Server ID: {serv_id}")
+        if serv_id in dic:
+            dic[serv_id] += 1
+        else:
+            dic[serv_id] = 1
+    
+    print(dic)
+    # plot the histogram of server ids
+    plt.bar(dic.keys(), dic.values(), color='g')
+    plt.show()
+    # save it to a file
+    plt.savefig("stats.png")
 
 if __name__ == '__main__':
-    # rm(1, ["s3"])
-    # add(3, ["s1", "s2", "s3"])
-    home()
+    asyncio.run(main())
