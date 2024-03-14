c20f7ec759f17d3b958cead795f664772fc334ff
Rushil Venkateswar <rushilv14@gmail.com>
2024-03-14 14:31:45 +0530

    kuch toh kiye hai

---
+++ A2/lb/consistent_hashing.py
@@ -0,0 +1,130 @@
+from sortedcontainers import SortedList
+
+class ConsistentHashMap:
+    """
+    This class consists of a consistent hash map with support for addition of new server instances and removal of existing server instances.
+    
+    Attributes:
+        num_server_containers: number of server containers
+        num_virtual_servers: number of virtual servers per server container
+        num_slots: number of slots in the consistent hash map
+    """
+
+    def __init__(self, num_server_containers : int = 3, num_virtual_servers = 9, num_slots = 512) -> None:
+        """
+        The constructor for ConsistentHashMap class.
+        :param num_server_containers: number of server containers
+        :param num_virtual_servers: number of virtual servers per server container
+        :param num_slots: number of slots in the consistent hash map
+        """
+        self.num_server_containers = num_server_containers
+        self.num_virtual_servers = num_virtual_servers
+        self.num_slots = num_slots
+
+        # we create two arrays, one for storing the node themselves, and the other for storing the index of each server in the consistent hash map
+        self.sorted_keys = SortedList()
+        self.ring = [None] * self.num_slots
+        # self.server_nodes = [] * self.num_server_containers
+        self.server_indexes = {}
+
+
+    def modulo(self, a: int, b: int) -> int:
+        """
+        Function implements modulo operation.
+        """
+        temp = a % b
+        if temp < 0:
+            temp += b
+            temp %= b
+        
+        return temp
+
+
+    def virt_serv_hash_func(self, i: int, j: int) -> int:
+        """
+        This function is used to calculate hash value for a virtual server.
+        :param i: server ID
+        :param j: virtual server replica ID of server i
+        :return: hash value
+        """
+        i, j = int(i), int(j)
+        hash_val = int(i**2 + j**2 + 2*j + 25)
+        return hash_val
+    
+
+    def req_hash_func(self, i: int) -> int:
+        """
+        This function is used to calculate hash value for a request.
+        :param i: request ID for a client request
+        :return: hash value 
+        """
+        i = int(i)
+        hash_val =  int(i**2 + 2*i + 17)
+        return hash_val
+
+ 
+    def __get_empty_slot__(self, hash_val) -> None:
+        """
+        This function implements quadratic probing to insert a virtual server instance into the consistent hash map.
+        :param hash_val: hash value of the virtual server instance
+        """
+        def probing_func(i: int) -> int:
+            return (hash_val + i**2)
+        
+        for i in range(self.num_slots):
+            slot = self.modulo(probing_func(i), self.num_slots)
+            
+            if self.ring[slot] == None:
+                return slot
+        
+        return -1
+
+
+    def add_server(self, server_id: int) -> None:
+        """
+        This function is used to add a new server instance to the consistent hash map.
+        :param server_id: ID of the new server instance
+        """
+        self.server_indexes[server_id] = [None] * self.num_virtual_servers
+
+        for i in range(self.num_virtual_servers):
+            temp_hash_val = self.virt_serv_hash_func(server_id, i)
+            hash_val = self.modulo(temp_hash_val, self.num_slots)
+
+            slot = self.__get_empty_slot__(hash_val)
+
+            if slot == -1:
+                raise Exception("No empty slot found in the consistent hash map.")
+
+            self.sorted_keys.add(slot)
+            self.ring[slot] = server_id
+            self.server_indexes[server_id][i] = slot
+
+
+    def remove_server(self, server_id: int) -> None:
+        """
+        This function is used to remove an existing server instance from the consistent hash map.
+        :param server_id: ID of the server instance to be removed
+        """
+        for i in range(self.num_virtual_servers):
+            slot = self.server_indexes[server_id][i]
+
+            self.sorted_keys.remove(slot)
+            self.ring[slot] = None
+            self.server_indexes[server_id][i] = None
+            
+
+    def get_server(self, request_id: int) -> int:
+        """
+        This function is used to get the server instance to which a request is routed.
+        :param request_id: ID of the request
+        :return: ID of the server instance to which the request is routed
+        """
+        hash_val = self.req_hash_func(request_id)
+        slot = self.modulo(hash_val, self.num_slots)
+
+        idx = self.sorted_keys.bisect_left(slot)
+        if idx == len(self.sorted_keys):
+            idx = 0
+        
+        return self.ring[self.sorted_keys[idx]]
--- A2/lb/load_balancer.py
+++ A2/lb/load_balancer.py
@@ -4,17 +4,35 @@ import asyncio
 import aiohttp
 import docker
 import random
-import os
 
 app = Quart(__name__)
+
+# locks and mutexes
+locks = asyncio.Lock()
+resource = 0
+readers_waiting = 0
+writers_waiting = 0
+write_in_progress = False
+writer_priority = True
+
+# other globals
+num_servers = 0
+num_shards = 0
+schema = None
+
 client = docker.from_env()
+ch = ConsistentHashMap()
 server_id_to_hostname = {}
 server_hostname_to_id = {}
 server_to_shards = {}
+shards_to_server = {}
 shard_to_data = {}
 network = "n1"
 image = "serv"
 schema = {}
+
+
+
 async def check_heartbeat(server_name = None):
     url = f'http://{server_name}:5000/heartbeat'
     try:
@@ -26,6 +44,7 @@ async def check_heartbeat(server_name = None):
                     return False
     except Exception as e:
         return False
+
 ## Abhi bas copy pasted hai isko theek karna hoga
 async def periodic_server_monitor(interval = 1):
     while True:
@@ -53,61 +72,86 @@ async def periodic_server_monitor(interval = 1):
 
 @app.route('/init', methods=['POST'])
 async def init(payload = None):
-    # NOTHING DONE ABOUT CONSISTENT HASHMAP
     payload = await request.get_json()
+    global num_servers
     num_servers = payload['N']
     servers = payload['servers']
     shards = payload['shards']
+    global num_shards
+    num_shards = len(shards)
     global schema
     schema = payload['schema']
+
+    # error checking
     if num_servers is None or servers is None or shards is None or schema is None:
         response = jsonify(message = "Invalid Payload, something is missing", status = 'failure')
         response.status_code = 400
         return response
+    
     if num_servers != len(servers):
         response = jsonify(message = "Number of servers does not match N", status = 'failure')
         response.status_code = 400
         return response
+    
+    # update global data structures
     for shard_data in shards:
         shard_to_data[shard_data["Shard_id"]] = shard_data 
+
     for server_name, shard_list in servers.items():
         if server_name not in server_hostname_to_id:
             server_id = random.randint(100000, 999999)
             server_id_to_hostname[server_id] = server_name
             server_hostname_to_id[server_name] = server_id
-            server_to_shards[server_name] = shard_list
-            try:
-                res = client.containers.run(image=image, name=server_name, network=network, detach=True, environment={'SERV_ID': server_id})
-            except Exception as e:
-                print(e)
-                response = jsonify(message = '<Error> Failed to spawn new docker container', 
-                                status = 'failure')
-                response.status_code = 400
-                return response
-            url = f'http://{server_name}:5000/config'
-            payload = {}
-            payload['schema'] = schema
-            payload['shards'] = shard_list
-            try:
-                async with aiohttp.ClientSession() as client_session:
-                    async with client_session.post(url,json = payload) as response:
-                        response_json = await response.json()
-                        print(response_json)
-            except Exception as e:
-                print(e)
-                response = jsonify(message = '<Error> Failed to configure server', status = 'failure')
-                response.status_code = 400
-                return response
-                        # return Response(content, status=response.status, headers = dict(response.headers))
+
+        server_to_shards[server_name] = shard_list
+        for shard in shard_list:
+            if shard not in shards_to_server:
+                shards_to_server[shard] = []
+            shards_to_server[shard].append(server_name)
+
+        try:
+            res = client.containers.run(image=image, name=server_name, network=network, detach=True, environment={'SERV_ID': server_id})
+        except Exception as e:
+            print(e)
+            response = jsonify(message = '<Error> Failed to spawn new docker container', status = 'failure')
+            response.status_code = 400
+            return response
+            
+        url = f'http://{server_name}:5000/config'
+        payload = {}
+        payload['schema'] = schema
+        payload['shards'] = shard_list
+
+        try:
+            async with aiohttp.ClientSession() as client_session:
+                async with client_session.post(url, json = payload) as response:
+                    response_json = await response.json()
+                    print(response_json)
+
+                    if response.status != 200:
+                        response = jsonify(message = f'<Error> Failed to configure server {server_name}', status = 'failure')
+                        response.status_code = 400
+                        return response
+
+        except Exception as e:
+            print(e)
+            response = jsonify(message = '<Error> Failed to configure server', status = 'failure')
+            response.status_code = 400
+            return response
+
     response = jsonify(message = "Configured Database", status = 'success')
     response.status_code = 200
     return response
+
+
 @app.route('/status', methods=['GET'])
 async def status(payload = None):
     response_content = {}
     response_content['N'] = len(server_hostname_to_id)
     response_content['servers'] = server_to_shards
     response_content['shards'] = shard_to_data.values()
+    response_content['schema'] = schema
+
     response = jsonify(message = response_content, status = 'success')
     response.status_code = 200
     return response
@@ -117,45 +161,69 @@ async def status(payload = None):
 async def add(payload = None):
     #checks lagana hai
     payload = await request.get_json()
-    num_new_servers = payload['n']  
+    num_new_servers = payload['n']
+    num_servers += num_new_servers
+
     new_shards = payload['new_shards']
+    num_shards += len(new_shards)
+
     servers = payload['servers']
+
     for shard_data in new_shards:
         shard_to_data[shard_data["Shard_id"]] = shard_data
+
     # if less than n toh randomly init serverid
-    for server_name,shard_list in servers:
+    message = "Add "
+    for server_name, shard_list in servers:
         # if already exists
         if server_name not in server_hostname_to_id:
             server_id = random.randint(100000, 999999)
             server_id_to_hostname[server_id] = server_name
             server_hostname_to_id[server_name] = server_id
-            server_to_shards[server_name] = shard_list
-            try:
-                res = client.containers.run(image=image, name=server_name, network=network, detach=True, environment={'SERV_ID': server_id})
-            except Exception as e:
-                print(e)
-                response = jsonify(message = '<Error> Failed to spawn new docker container', 
-                                status = 'failure')
-                response.status_code = 400
-                return response
-            url = f'http://{server_name}:5000/config'
-            payload = {}
-            payload['schema'] = schema
-            payload['shards'] = shard_list
-            try:
-                async with aiohttp.ClientSession() as client_session:
-                    async with client_session.post(url,json = payload) as response:
-                        response_json = await response.json()
-                        print(response_json)
-            except Exception as e:
-                print(e)
-                response = jsonify(message = '<Error> Failed to configure server', status = 'failure')
-                response.status_code = 400
-                return response
+            
+        server_to_shards[server_name] = shard_list
+        for shard in shard_list:
+            if shard not in shards_to_server:
+                shards_to_server[shard] = []
+            shards_to_server[shard].append(server_name)
+
+        try:
+            res = client.containers.run(image=image, name=server_name, network=network, detach=True, environment={'SERV_ID': server_id})
+        except Exception as e:
+            print(e)
+            response = jsonify(message = '<Error> Failed to spawn new docker container', status = 'failure')
+            response.status_code = 400
+            return response
+
+        url = f'http://{server_name}:5000/config'
+        payload = {}
+        payload['schema'] = schema
+        payload['shards'] = shard_list
+        try:
+            async with aiohttp.ClientSession() as client_session:
+                async with client_session.post(url,json = payload) as response:
+                    response_json = await response.json()
+                    print(response_json)
+
+                    if response.status != 200:
+                        response = jsonify(message = f'<Error> Failed to configure server {server_name}', status = 'failure')
+                        response.status_code = 400
+                        return response
+
+        except Exception as e:
+            print(e)
+            response = jsonify(message = '<Error> Failed to configure server', status = 'failure')
+            response.status_code = 400
+            return response
+        
+        message += server_name + ", "
+    message = message[:-2]
+    
     response_json = {}
     response_json['N'] = len(server_hostname_to_id)
-    response_json['message'] = "Added new servers"# idhar list of servers bhi bhejna hai
+    response_json['message'] = message
     response_json['status'] = 'successful'
+
     response = jsonify(message = response_json, status = 'success')
     response.status_code = 200
     return response
@@ -168,15 +236,13 @@ async def rm(payload = None):
     rm_servers = payload['servers']
 
     if num_rm_servers is None or rm_servers is None:
-        response = jsonify(message = '<Error> Invalid payload', 
-                    status = 'failure')
+        response = jsonify(message = '<Error> Invalid payload', status = 'failure')
         response.status_code = 400
         return response
 
     # if there are more servers in rm_servers than the number of servers in the network, then throw error
     if num_rm_servers < len(rm_servers):
-        response = jsonify(message = '<Error> Length of hostname list is more than removable instances', 
-                    status = 'failure')
+        response = jsonify(message = '<Error> Length of hostname list is more than removable instances', status = 'failure')
         response.status_code = 400
         return response
     
@@ -184,8 +250,7 @@ async def rm(payload = None):
     containers = server_hostname_to_id.keys()
     for server in rm_servers:
         if server not in containers:
-            response = jsonify(message = '<Error> Server not found', 
-                        status = 'failure')
+            response = jsonify(message = '<Error> Server not found', status = 'failure')
             response.status_code = 400
             return response
 
@@ -196,6 +261,7 @@ async def rm(payload = None):
 
         # randomly select num_needed servers from other_servers
         rm_servers += random.sample(other_servers, num_needed)
+
     for server in rm_servers:
         # remove the server from the consistent hash map
         server_id = server_hostname_to_id[server]
@@ -219,7 +285,7 @@ async def rm(payload = None):
 
     message = {
         'N': len(containers),
-        'replicas': list(containers)
+        'servers': list(containers)
     }
 
     response = jsonify(message = message, status = 'successful')
@@ -231,9 +297,45 @@ async def rm(payload = None):
 async def read(payload = None):
     payload = await request.get_json()
 
+    global resource, readers_waiting, writers_waiting, write_in_progress, writer_priority
+    await lock.acquire()
+    while write_in_progress or (writer_priority and writers_waiting > 0):
+        readers_waiting += 1
+        lock.release()
+        await asyncio.sleep(0.1)
+        await lock.acquire()
+        readers_waiting -= 1
+    lock.release()
+
+    # critical section for reader
+
+    response = jsonify(message = '.', status = 'success')
+    response.status_code = 200
+    return response
+
 @app.route('/write', methods=['POST'])
 async def write(payload = None):
     payload = await request.get_json()
+
+    global resource, readers_waiting, writers_waiting, write_in_progress, writer_priority
+    await lock.acquire()
+    writers_waiting += 1
+    while write_in_progress or (readers_waiting > 0):
+        lock.release()
+        await asyncio.sleep(0.1)
+        await lock.acquire()
+    writers_waiting -= 1
+    write_in_progress = True
+    lock.release()
+
+    # critical section for reader
+
+    resource += 1
+    write_in_progress = False
+
+    response = jsonify(message = '.', status = 'success')
+    response.status_code = 200
+    return response
 
 @app.route('/update', methods=['PUT'])
 async def update(payload = None):
