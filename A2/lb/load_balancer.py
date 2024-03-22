136941591a9471b87ce5866f00a55ea3dd189a51
Shivam751 <shivsrj2580@gmail.com>
2024-03-22 23:19:32 +0530

    chutiyo khud kuch kar lo

--- A2/lb/load_balancer.py
+++ A2/lb/load_balancer.py
@@ -8,12 +8,12 @@ import random
 app = Quart(__name__)
 
 # locks and mutexes
-locks = asyncio.Lock()
-resource = 0
-readers_waiting = 0
-writers_waiting = 0
-write_in_progress = False
-writer_priority = True
+locks = {}
+resource = {}
+readers_waiting = {}
+writers_waiting = {}
+write_in_progress = {}
+writer_priority = {}
 
 # other globals
 num_servers = 0
@@ -31,7 +31,35 @@ network = "n1"
 image = "serv"
 schema = {}
 
-
+# def get_shard_id(stud_id):
+#     for shard_data in shard_to_data:
+#         stud_id_low = shard_data["Stud_id_low"]
+#         shard_size = shard_data["Shard_size"]
+#         if stud_id >= stud_id_low and stud_id < stud_id_low + shard_size:
+#             return shard_data["Shard_id"]
+#     return None  # If no shard matches
+async def before_critical_write(shard_id):
+    global resource, readers_waiting, writers_waiting, write_in_progress, writer_priority
+    await locks[shard_id].acquire()
+    writers_waiting[shard_id] += 1
+    while write_in_progress[shard_id] or (readers_waiting[shard_id] > 0):
+        locks[shard_id].release()
+        await asyncio.sleep(0.1)
+        await locks[shard_id].acquire()
+    writers_waiting[shard_id] -= 1
+    write_in_progress[shard_id] = True
+    locks[shard_id].release()
+    return
+async def after_critical_write(shard_id):
+    write_in_progress[shard] = False
+# logn wala implement kar dena uske liye dictionary ko array banana padega
+def get_shard_id(stud_id):
+    for shard_id, shard_info in shard_to_data.items():
+        stud_id_low = shard_info["Stud_id_low"]
+        shard_size = shard_info["Shard_size"]
+        if stud_id_low <= stud_id < stud_id_low + shard_size:
+            return shard_id
+    return None  # If student ID doesn't belong to any shard
 
 async def check_heartbeat(server_name = None):
     url = f'http://{server_name}:5000/heartbeat'
@@ -311,16 +339,15 @@ async def read(payload = None):
     
     # retrieve data from the shards
     for shard_id in shard_ids:
-        lock = locks[shard_id]
         global resource, readers_waiting, writers_waiting, write_in_progress, writer_priority
-        await lock.acquire()
-        while write_in_progress or (writer_priority and writers_waiting > 0):
-            readers_waiting += 1
-            lock.release()
+        await locks[shard_id].acquire()
+        while write_in_progress[shard_id] or (writer_priority and writers_waiting[shard_id] > 0):
+            readers_waiting[shard_id] += 1
+            locks[shard_id].release()
             await asyncio.sleep(0.1)
-            await lock.acquire()
-            readers_waiting -= 1
-        lock.release()
+            await locks[shard_id].acquire()
+            readers_waiting[shard_id] -= 1
+        locks[shard_id].release()
 
         # critical section for reader
         # read data for 
@@ -332,41 +359,132 @@ async def read(payload = None):
 @app.route('/write', methods=['POST'])
 async def write(payload = None):
     payload = await request.get_json()
+    shard = payload['shard']
+    stud_id = payload['Stud_id']
+    data = payload['data']
+    await before_critical_write(shard)
 
+    # critical section for write
+    for server in shards_to_server[shard]:
+        server_name = server_id_to_hostname[server]
+        url = f'http://{server_name}:5000/write'
+        payload = {}
+        payload['shard'] = shard
+        payload['data'] = data
 
-    # identify the shard_ids from the student_ids in the payload
+        try:
+            async with aiohttp.ClientSession() as client_session:
+                async with client_session.post(url, json = payload) as response:
+                    response_json = await response.json()
+                    print(response_json)
 
+                    if response.status != 200:
+                        response = jsonify(message = f'<Error> Failed to write to server {server_name}', status = 'failure')
+                        response.status_code = 400
+                        after_critical_write(shard)
+                        return response
 
-    global resource, readers_waiting, writers_waiting, write_in_progress, writer_priority
-    await lock.acquire()
-    writers_waiting += 1
-    while write_in_progress or (readers_waiting > 0):
-        lock.release()
-        await asyncio.sleep(0.1)
-        await lock.acquire()
-    writers_waiting -= 1
-    write_in_progress = True
-    lock.release()
+        except Exception as e:
+            print(e)
+            response = jsonify(message = '<Error> Failed to write to server', status = 'failure')
+            response.status_code = 400
+            after_critical_write(shard)
+            return response
 
-    # critical section for reader
+    after_critical_write(shard)
 
-    resource += 1
-    write_in_progress = False
-
-    response = jsonify(message = '.', status = 'success')
+    response = jsonify(message = f'{len(data)} Data entries added', status = 'success')
     response.status_code = 200
     return response
 
 @app.route('/update', methods=['PUT'])
 async def update(payload = None):
     payload = await request.get_json()
+    stud_id = payload['Stud_id']
+    data = payload['data']
+    shard_id = get_shard_id(stud_id)
+    if shard_id is None:
+        response = jsonify(message = '<Error> Student ID not found', status = 'failure')
+        response.status_code = 400
+        return response
+    
+    await before_critical_write(shard_id)
+    
+    # critical section for write
+    for server in shards_to_server[shard_id]:
+        server_name = server_id_to_hostname[server]
+        url = f'http://{server_name}:5000/update'
+        payload = {}
+        payload['shard'] = shard_id
+        payload['Stud_id'] = stud_id
+        payload['data'] = data
+
+        try:
+            async with aiohttp.ClientSession() as client_session:
+                async with client_session.put(url, json = payload) as response:
+                    response_json = await response.json()
+                    print(response_json)
+
+                    if response.status != 200:
+                        response = jsonify(message = f'<Error> Failed to update server {server_name}', status = 'failure')
+                        response.status_code = 400
+                        after_critical_write(shard_id)
+                        return response
+
+        except Exception as e:
+            print(e)
+            response = jsonify(message = '<Error> Failed to update server', status = 'failure')
+            response.status_code = 400
+            after_critical_write(shard_id)
+            return response
+
+    after_critical_write(shard_id)
+
+    response = jsonify(message = f'Data entry for Stud_id{stud_id} updated', status = 'success')
+    
 
 @app.route('/del', methods=['DELETE'])
 async def delete(payload = None):
     payload = await request.get_json()
+    stud_id = payload['Stud_id']
+    shard_id = get_shard_id(stud_id)
 
+    if shard_id is None:
+        response = jsonify(message = '<Error> Student ID not found', status = 'failure')
+        response.status_code = 400
+        return response
+    
+    await before_critical_write(shard_id)
 
+    # critical section for write
+    for server in shards_to_server:
+        server_name = server_id_to_hostname[server]
+        url = f'http://{server_name}:5000/del'
+        payload = {}
+        payload['shard'] = shard_id
+        payload['Stud_id'] = stud_id
 
+        try:
+            async with aiohttp.ClientSession() as client_session:
+                async with client_session.delete(url, json = payload) as response:
+                    response_json = await response.json()
+                    print(response_json)
+
+                    if response.status != 200:
+                        response = jsonify(message = f'<Error> Failed to delete server {server_name}', status = 'failure')
+                        response.status_code = 400
+                        after_critical_write(shard_id)
+                        return response
+
+        except Exception as e:
+            print(e)
+            response = jsonify(message = '<Error> Failed to delete server', status = 'failure')
+            response.status_code = 400
+            after_critical_write(shard_id)
+            return response
+    after_critical_write(shard_id)
+
+    response = jsonify(message = f'Data entry with Stud_id{stud_id} removed', status = 'success')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
