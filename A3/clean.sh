99e164c841ef33d77205df9ebbd80bd9bfc84bf1
Rushil Venkateswar <rushilv14@gmail.com>
2024-04-17 00:45:11 +0530

    changed makefile

--- A3/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker stop $(docker ps -a | grep -i server | awk '{print $1}')
-docker rm $(docker ps -a | grep -i server | awk '{print $1}')
-docker stop lb metadata shardmanager
-docker rm lb metadata shardmanager
-docker network rm net1
