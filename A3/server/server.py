f12eea28631ffac334a846a9abd52ebe6445866c
Shivam751 <shivsrj2580@gmail.com>
2024-04-25 20:43:01 +0530

    server code

--- A3/server/server.py
+++ A3/server/server.py
@@ -62,19 +62,17 @@ def configure_server():
     if not schema or not shards:
         return jsonify({"message": "Invalid payload", "status": "error"}), 400
 
-    if 'columns' not in schema or 'dtypes' not in schema or len(schema['columns']) != len(schema['dtypes']) or len(schema['columns']) == 0:
+    if 'columns' not in schema or 'dtypes' not in schema:
         return jsonify({"message": "Invalid schema", "status": "error"}), 400
 
+    if len(schema['columns']) != len(schema['dtypes']) or len(schema['columns']) == 0:
+        return jsonify({"message": "No columns or dtypes", "status": "error"}), 400
+    
     for shard in shards:
         sql.UseDB(dbname=shard)
-        sql.CreateTable(
-            tabname='studT', columns=schema['columns'], dtypes=schema['dtypes'], prikeys=['Stud_id'])
+        sql.CreateTable(tabname='studT', columns=schema['columns'], dtypes=schema['dtypes'], prikeys=['Stud_id'])
 
-    response_message = f"{', '.join(f'{server_name}:{shard}' for shard in shards)} configured"
-    response_data = {"message": response_message, "status": "success"}
-
-    return jsonify(response_data), 200
-
+    return jsonify({"message": f"{', '.join(f'{server_name}:{shard}' for shard in shards)} configured", "status": "success"}), 200
 
 @app.route('/copy', methods=['GET'])
 def copy_data():
@@ -87,7 +85,7 @@ def copy_data():
     response_data = {}
     for shard in shards:
         if not sql.hasDB(dbname=shard):
-            return jsonify({"message": f"{server_name}:{shard} not found", "status": "error"}), 404
+            return jsonify({"message": f"{server_name}:{shard} NOT FOUND", "status": "error"}), 404
         sql.UseDB(dbname=shard)
         response_data[shard] = sql.Select(table_name='studT')
     response_data["status"] = "success"
@@ -100,29 +98,28 @@ def read_data():
     print("Read route")
     payload = request.get_json()
     shard = payload.get('shard')
-    Stud_id = payload.get('Stud_id')
+    stud_id = payload.get('Stud_id')
 
-    if not shard or not Stud_id or 'low' not in Stud_id or 'high' not in Stud_id:
+    if not shard or not stud_id or 'low' not in stud_id or 'high' not in stud_id:
         return jsonify({"message": "Invalid payload", "status": "error"}), 400
 
     if not sql.hasDB(dbname=shard):
         return jsonify({"message": f"{server_name}:{shard} not found", "status": "error"}), 404
 
     sql.UseDB(dbname=shard)
-    data = sql.Select(table_name="studT", col="Stud_id",
-                      low=Stud_id['low'], high=Stud_id['high'])
 
-    response_data = {"data": data, "status": "success"}
+    response_data = {"data": sql.Select(table_name="studT", col="stud_id", low=stud_id['low'], high=stud_id['high']), "status": "success"}
 
     return jsonify(response_data), 200
 
 @app.route('/write', methods=['POST'])
 async def write_data():
     global server_name
-    print(server_name)
+
     payload = request.get_json()
     shard = payload.get('shard') #"sh1"
     data = payload.get('data')  #[{"Stud_id": 2255, "Stud_name": "GHI", "Stud_marks": 27}]
+
     if not shard or not data:
         return jsonify({"message": "Invalid payload", "status": "error"}), 400
     
@@ -131,39 +128,34 @@ async def write_data():
     
     if shard not in logfile:
         logfile[shard] = []
+
     logfile[shard].append(["WRITE", data, len(logfile[shard]) + 1])
 
-    print("hi2")
     file = open(WAL, "a")
     for entry in data:
         file.write(f"WRITE {shard}, {entry['Stud_id']}, {entry['Stud_name']}, {entry['Stud_marks']}\n")
+
     shard_to_logcount[shard] = shard_to_logcount.get(shard, 0) + len(data)
     file.close()
     
-    # this server is a primary server if shard is in primary_shards
     if shard in primary_shards:
         print(f"{server_name} is a primary server for shard {shard}")
-        print("hello in loop")
         servers = await get_shard_servers(shard)
-        print(servers)
-        print("hello")
         if servers is None:
             return jsonify({"message": f"Error in getting servers for {shard}", "status": "error"}), 500
         for server in servers:
             if server == server_name:
                 print("same server found")
                 continue
-            response = await write_shard_data(server, shard, data)
-            print("hi there")
+            response = await write_shard_data(server, shard, data) 
             if response is False:
                 return jsonify({"message": f"Error in writing data to {server}", "status": "error"}), 500 
 
-        print("hi4")
 
     sql.UseDB(dbname=shard)
     sql.Insert(table_name='studT', rows=data)
 
-    response_data = {"message": "Data entries added", "status": "success"}
+    response_data = {"message": "Data entries written to database", "status": "success"}
     return jsonify(response_data), 200
 
 
@@ -180,16 +172,14 @@ async def update_data():
     if not sql.hasDB(dbname=shard):
         return jsonify({"message": f"{server_name}:{shard} not found", "status": "error"}), 404
 
-    print("Logging ke pehele")
     if shard not in logfile:
         logfile[shard] = []
+
     logfile[shard].append(["UPDATE", data, len(logfile[shard]) + 1])
-    print("Logging ke baad")
     file = open(WAL, "a")
     file.write(f"UPDATE {shard}, {Stud_id}, {data['Stud_name']}, {data['Stud_marks']}\n")
     file.close()
 
-    print("Ab loop enter karenge")
     shard_to_logcount[shard] = shard_to_logcount.get(shard, 0) + 1
     if shard in primary_shards:
         servers = await get_shard_servers(shard)
@@ -202,16 +192,12 @@ async def update_data():
             response = await update_shard_data(server, shard, Stud_id, data)
             if response is False:
                 return jsonify({"message": f"Error in writing data to {server}", "status": "error"}), 500
-    print("Loop khatam")
     sql.UseDB(dbname=shard)
     if not sql.Exists(table_name='studT', col="Stud_id", val=Stud_id):
         return jsonify({"message": f"Data entry for Stud_id:{Stud_id} not found", "status": "error"}), 404
+    
     sql.Update(table_name='studT', col="Stud_id", val=Stud_id, data=data)
-    print("Data updated")
-    response_data = {
-        "message": f"Data entry for Stud_id:{Stud_id} updated", "status": "success"
-    }
-    print("Ab return karenge update ko")
+    response_data = { "message": f"Data entry for Stud_id:{Stud_id} updated", "status": "success" }
     return jsonify(response_data), 200
 
 
@@ -229,6 +215,7 @@ async def delete_data():
 
     if shard not in logfile:
         logfile[shard] = []
+
     logfile[shard].append(["DELETE", {"Stud_id": Stud_id}, len(logfile[shard]) + 1])
 
     file = open(WAL, "a")
@@ -252,8 +239,7 @@ async def delete_data():
         return jsonify({"message": f"Data entry for Stud_id:{Stud_id} not found", "status": "error"}), 404
     sql.Delete(table_name='studT', col="Stud_id", val=Stud_id)
 
-    response_data = {
-        "message": f"Data entry with Stud_id:{Stud_id} removed", "status": "success"}
+    response_data = {"message": f"Data entry with Stud_id:{Stud_id} removed", "status": "success"}
 
     return jsonify(response_data), 200
 
@@ -262,8 +248,7 @@ def get_log_count():
     payload = request.get_json()
     shard = payload.get('shard')
 
-    logcount = shard_to_logcount.get(shard, 0)
-    return jsonify({"server_name": server_name, "logcount": logcount, "status": "success"}), 200
+    return jsonify({"server_name": server_name, "logcount": shard_to_logcount.get(shard, 0), "status": "success"}), 200
 
 @app.route('/get_log', methods=['GET'])
 def get_log():
